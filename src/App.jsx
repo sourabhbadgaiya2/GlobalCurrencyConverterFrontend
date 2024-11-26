9a37decef81bce62434d4e0add77244262ca0c29
sourabhbadgaiya2 <sourabhbadgaiya2@gmail.com>
2024-11-26 12:41:05 +0530

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,8 @@ const App = () => {
   };
 
   const handleSubmit = async (e) => {
+    console.log("clicked");
+
     e.preventDefault();
     try {
       const response = await axios.post(
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Expose the server to the network
+    port: process.env.PORT || 5173, // Use the Render-provided port or default to 5173
+  },
+});
